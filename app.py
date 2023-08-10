f20cb21199645f820d60ecdcf6601cd5bba0d700
Santiago Olarte <saolarte@msn.com>
2023-08-10 14:57:15 -0500

    pass bucket name and client to upload file function

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ BASE_URL = "https://api.nasa.gov/planetary/earth/imagery"
 BUCKET_NAME = "test-bucket-san9405"
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
 S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
-s3_client = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
 
 
 
@@ -43,11 +42,11 @@ def read_csv(file_location):
     return fields
 
 
-def upload_file_to_s3(file_url, field_data):
+def upload_file_to_s3(s3_client, file_url, field_data, bucket=BUCKET_NAME):
     today = date.today()
     file_destination = f"{field_data['field_id']}/{today.strftime('%Y%m%d')}_imagery.png"
     try:
-        response = s3_client.upload_file(file_url, BUCKET_NAME, file_destination)
+        response = s3_client.upload_file(file_url, bucket, file_destination)
     except Exception as error:
         logging.error(f"Error uploading file to S3: {str(error)}")
         return False
